06fcbc1e8ff5d061bde293e1c07c6eb1f7dd802f
Peter Schilling <peter@peterschilling.org>
2022-12-07 13:49:57 -0800

    demo!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def example_1
+    tag.ul do
+      tag.li do
+        "example 1, i work!"
+      end
+    end
+  end
+
+  def example_2
+    tag.ul do
+      tag.li do
+        "example 2, i am never seen! it would be better if i was!"
+      end
+
+      tag.li do
+        "example 2, i get printed!"
+      end
+    end
+  end
 end
